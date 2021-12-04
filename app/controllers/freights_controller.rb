6b2a8656a966a58928f4aeae6116d6e9ad306b7c
Juan Salazar <jc.salazargomez@gmail.com>
2021-12-04 11:03:04 -0500

    freight controller

--- app/controllers/freights_controller.rb
+++ app/controllers/freights_controller.rb
@@ -1,2 +1,39 @@
 class FreightsController < ApplicationController
+  def show
+    @freight = Freight.find(params[:id])
+    @review = Review.new
+    @reviews = Review.where(trip_id: params[:id])
+    @reservations = Reservation.where(trip_id: params[:id])
+  end
+
+  def new
+    @freight = Freight.new
+  end
+
+  def create
+    @freight = Freight.new(freight_params)
+    @freight.save
+    redirect_to freight_path(@freight)
+  end
+
+  def index
+    if params[:query].present?
+      @freight = Freight.where("destination ILIKE ?", "%#{params[:query]}%")
+    else
+      @freight = Freight.all
+    end
+  end
+
+  private
+
+  def freight_params
+    params.require(:freight).permit(
+      :date,
+      :departure,
+      :destination,
+      :price,
+      :departure_time,
+      :estimated_time_arrival
+    )
+  end
 end

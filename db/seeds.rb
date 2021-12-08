191f40a2094752c648dd3a23f2d87f076d4f047e
Rocio Romero <rocio.aldanaromero@gmail.com>
2021-12-07 22:46:11 -0300

    Estilo navbar y seeds de freights

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Freight.destroy_all
+Truck.create(capacity: "5.000 toneladas", model: "222", user_id: 1)
+Freight.create(date: Date.new(2022, 2, 2), departure: "Santa Fe", destination: "Buenos Aires", price: 149.00, departure_time: "5:00 P.M.", estimated_time_arrival: "01:00 A.M.", truck_id: 1, description: "DDD")
+Freight.create(date: Date.new(2021, 12, 12), departure: "Santa Fe", destination: "Buenos Aires", price: 149.00, departure_time: "5:00 P.M.", estimated_time_arrival: "01:00 A.M.", truck_id: 1, description: "DDD")
+Freight.create(date: Date.new(2022, 5, 5), departure: "Cancun", destination: "Ciudad de Mexico", price: 200.00, departure_time: "6:00 A.M.", estimated_time_arrival: "10:00 P.M.", truck_id: 1, description: "DDD")
+Freight.create(date: Date.new(2022, 4, 4), departure: "Cancun", destination: "Ciudad de Mexico", price: 200.00, departure_time: "6:00 A.M.", estimated_time_arrival: "10:00 P.M.", truck_id: 1, description: "DDD")
+Freight.create(date: Date.new(2022, 6, 6), departure: "Cancun", destination: "Ciudad de Mexico", price: 200.00, departure_time: "6:00 A.M.", estimated_time_arrival: "10:00 P.M.", truck_id: 1, description: "DDD")

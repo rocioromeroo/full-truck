5e12bce4d4b8deb30c55d8b078eb5a423e20d7ab
Rocio Romero <rocio.aldanaromero@gmail.com>
2021-12-11 12:04:51 -0300

    Rutas para about y contact, linkeo de rutas en el navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :freights, :reservations, :reviews, :trucks
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 end

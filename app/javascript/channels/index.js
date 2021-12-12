5874c36eed64c002b082159e1195d93e2243b4b4
AimeeSp <aijespreifersucht@hotmail.com>
2021-12-12 15:56:25 -0600

    registro

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,20 +1,6 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
-import React from "react";
-import ReactDOM from "react-dom";
 import  BrowserRouter  from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById("root")
-);
-
-serviceWorker.unregister();
-
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
